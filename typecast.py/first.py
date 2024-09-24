819d933672f577fa46d839bfc2a3f7d52068a8e9
Manminder <ManminderSingh@edu.sait.ca>
2024-09-24 16:11:07 -0600

    doingittttttt

---
+++ typecast.py/first.py
@@ -0,0 +1,12 @@
+# typecasting in python
+# Typecasting is the process of converting one data type to another data type.
+
+# integers
+# float
+# string
+z = int ( 100)
+y =  float (100.0)
+x = str ( "2, s1")
+print(type(z))
+print(type(y))
+print(type(x))
