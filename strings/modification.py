790e0261d73f28b5ac81e395b37bfd54bab980cb
Manminder <ManminderSingh@edu.sait.ca>
2024-09-24 16:48:03 -0600

    modification

---
+++ strings/modification.py
@@ -0,0 +1,56 @@
+# uppercase 
+# Description: This function returns a string with all the characters in uppercase.
+
+# Syntax: string.upper()
+a = "manminder"
+print(a.upper())
+# it will print MANMINDER because it will convert all the characters in uppercase
+
+
+# lowercase
+# Description: This function returns a string with all the characters in lowercase.
+
+# Syntax: string.lower()
+a = "MANMINDER"
+print(a.lower())
+# it will print manminder because it will convert all the characters in lowercase
+
+# remove whitespace
+# Description: This function returns a string with all the leading and trailing whitespace removed.
+
+# Syntax: string.strip()
+a = " manminder "
+print(a.strip)
+# it will print manminder because it will remove the leading and trailing whitespace
+
+# replace string
+# Description: This function returns a string with a specified value replaced with another value.
+
+# Syntax: string.replace(oldvalue, newvalue)
+
+a = "manminder"
+print(a.replace("m", "M"))
+# it will print ManMinder because it will replace m with M      
+
+# split string
+# Description: This function splits a string into a list.
+
+# Syntax: string.split(separator, maxsplit)
+
+a = "manminder singh"
+print(a.split(" {}"))
+# it will print ['manminder', 'singh'] because it will split the string into two parts
+
+
+
+
+# check string
+# Description: This function checks if all the characters in the text are alphanumeric.
+
+# Syntax: string.isalnum()
+
+a = "manminder"
+print(a.isalnum())
+# it will print true because all the characters are alphanumeric
+
+
