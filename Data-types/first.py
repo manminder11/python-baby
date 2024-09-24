4dcc987d72c64c8f6f583bb502fe19fdac6f7ba3
Manminder <ManminderSingh@edu.sait.ca>
2024-09-24 12:58:15 -0600

    data types

---
+++ Data-types/first.py
@@ -0,0 +1,44 @@
+
+# built in data types
+# 1. int (int and long) 
+# 2. float   (float and decimal)
+# 3. complex (complex and decimal) 
+# 4. bool (bool and None) 
+# 5. str (str and bytes)
+# 6. list (list and deque) 
+# 7. tuple (tuple and namedtuple)
+# 8. set (set and frozenset)
+# 9. dict (dict and OrderedDict) 
+
+
+# gettype() function
+# type() function
+# isinstance function 
+
+int = 100
+float = 100.0
+print(type(int))
+print(type(float))
+
+# setting the type of the variable
+int = 100
+float = 100.0
+x = 1j 
+bool = True
+str = "hello"
+list = [1,2,3]
+tuple = (1,2,3)
+set = {1,2,3}
+dict = {"name":"john", "age":36}
+frozenset = frozenset({1,2,3})
+print(type(int))
+print(type(float))
+print(type(x))
+print(type(bool))
+print(type(str))
+print(type(list))
+print(type(tuple))
+print(type(set))
+print(type(dict))
+
+
