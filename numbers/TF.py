71f2b48fb487b598ef5d381d5a39c325f6a3c45a
Manminder <ManminderSingh@edu.sait.ca>
2024-09-24 19:02:44 -0600

    booleans

---
+++ numbers/TF.py
@@ -0,0 +1,40 @@
+# booleans in python are True and False
+# True is equal to 1
+# False is equal to 0
+
+# example
+print(10 < 4)
+# it will print False because 10 is not less than 4
+
+# example
+print(10 > 4)
+# it will print True because 10 is greater than 4
+
+# evaluate values and variables
+
+# example
+print(bool("hello"))
+# it will print True because the string is not empty
+
+# example
+print(bool(0))
+# it will print False because 0 is equal to False
+
+# example
+x = 10
+print(bool(x))
+# it will print True because x is not empty
+
+# false values \
+# The following will return False:
+#
+# Any string is False if it is empty.
+
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+# they all are false values
